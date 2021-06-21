266d18356027a9be68e959137873ea3a39bf5533
Dima-1987 <dima1987.dp.ua.ru@gmail.com>
2021-06-21 19:55:29 +0300

    Selenium указан аргумент запуска браузера chrome, изменен браузер с firefox на chrome

--- google_search_test.js
+++ google_search_test.js
@@ -1,5 +1,5 @@
 let webdriver = require('selenium-webdriver'),
-    //chrome = require('selenium-webdriver/chrome'),
+    chrome = require('selenium-webdriver/chrome'),
     By = webdriver.By,
     until = webdriver.until;
  //   test = require('selenium-webdriver/testing');
@@ -9,8 +9,12 @@ describe('Google Search', function () //создание главной функ
     let driver;      // объявляем переменную для работы с браузером
     before(function () //функция содержащая параметры запуска браузера
     {
+        let options = new chrome.Options; //создаем объект
+        options.addArguments(["start-fullscreen"]) //указываем аргумент
+
       driver = new webdriver.Builder()
-          .forBrowser('firefox') //указываем какой браузер запускаться
+          .forBrowser('chrome') //указываем какой браузер запускаться
+          .setChromeOptions(options) //применяем аргумент
           .build(); //Создается объект браузера
     });
 
